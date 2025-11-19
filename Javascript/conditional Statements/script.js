2b8107aaa0f3dc82fc8e1b25828a0338e5e5c5f1
Durgapraveen71 <durgapraveenias@gmail.com>
2025-11-20 01:38:46 +0530

    Javascript and other css properties

---
+++ Javascript/conditional Statements/script.js
@@ -0,0 +1,108 @@
+/*check if a number is greater than the other
+let num1 =5;
+let num2=6;
+if(num1<num2)
+{
+    console.log('num1 is greater')
+} */
+
+
+
+ /* check if a string is equal to another string or not 
+ 
+ let a = "praveen";
+ let b= " durga";
+
+ if(a===b){
+    console.log("Both strings are equal")
+ }
+ else{
+
+    console.log("Both strings are not equal")
+
+ }  */
+
+ 
+
+/*check if variable is number or not
+
+  var a ='10';
+  
+  if(typeof(a) === 'number'){
+    console.log('a is a number')
+  }
+
+  else {
+    console.log('a is not a number')
+  } */
+
+
+/*checking if a boolean value is true or false
+
+let isMaggieReady = false;
+if(isMaggieReady){
+    console.log('Ready to eat')
+}
+else{
+    console.log('Maggie is not ready')
+} */
+
+
+/*check if an araay is empty or not
+
+let array1 = ['prem', 'durga', 'praveen']
+
+if(array1.length==='null'){
+    console.log('Array is empty')
+}
+else{
+    console.log('array is not empty')
+} */
+
+
+ /*declare a =n array with the cricketrs name and accessthe first element in a variable and print inconsole
+ 
+ let cricketers= ['Shreyas', 'Iyer', 'Shashank']
+
+let res= cricketers[0];
+
+console.log(res) */
+
+
+/*Declare an array of your fav cities and access the third element in an element and store it and dispaly
+
+let cities= ['Hyd', 'Vij', 'Gun', 'Mtm', 'Kurn'];
+let  Storavalue = cities[2];
+
+console.log(Storavalue) */
+
+
+
+/*You have an array named dosas, replace ghee karam dosa with the ravva dosa
+
+let dosa = ['eggdosa', 'ravvadosa', 'gjeekaramdosa', 'masaladosa', 'pannerdosa'];
+
+
+console.log(dosa);
+
+dosa[2]= 'minapadosa';
+
+console.log(dosa); */
+
+
+
+//we have array of cities and need to add one more city in an array by push
+
+let cities = ['Hyd', 'Vij', 'Gun', 'Bnglr', 'Tn'];
+
+cities.push('Bihar');
+
+console.log(cities)
+
+// remove the last element in an array
+
+let city = ['Delhi', 'Mumbai', 'kolkata', 'rajasthan']
+
+city.pop();
+
+console.log(city);
---
+++ Javascript/script.js
@@ -0,0 +1,37 @@
+/*var num1 = prompt("Enter num1");
+var num2 =prompt("Enter num2");
+
+console.log(num1+num2);
+
+//Type casting-> so here by using real1, we have converted from string to number
+
+var real1 =Number(num1);
+var real2 = Number(num2);
+console.log(real1);
+console.log(real2);
+console.log(real1+real2); */
+
+/*var num1= 10;
+var num2 =20;
+console.log(num1+num2);
+
+var real1 = String(num1);
+var real2 = String(num2)
+console.log(real1+real2)
+
+console.log('Hello World'); */
+
+                                // Objects//
+                const username = {
+                    firstname: 'Durgapraveen',
+                    secondname:'Panthangi',
+                    age: 26,
+                    city: 'Hyderabad'
+                }
+                username.secondname = 'durga'
+
+                //printing the value in object
+                console.log(username.age);
+                console.log(username.firstname);
+                console.log(username.secondname);
+                console.log(username.city);
